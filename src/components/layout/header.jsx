bb30622fd9244daab669539d9ef0e76eb5f9b36e
Catto Francois <catto.francois@hotmail.fr>
2024-01-04 15:13:46 +0100

    modifs

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -15,34 +15,34 @@ import Link from 'next/link';
 function header() {
     return (
         <>
-      <header className=' sticky top-0 bg-vert text-ocre rounded-xl z-10 py-6 m-5  '>
+      <header className=' sticky top-0 bg-vert text-ocre rounded-xl z-10 py-6 sm:m-5  '>
         <nav className='  container flex items-center justify-between'>
           {/* Logo */}
           <div>
             <Link
               href='/'
-              className=' grow transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex items-center text-[3px] font-bold uppercase tracking-widest sm:text-sm lg:text-2xl'
+              className=' grow transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 flex items-center text-[6px] sm:text-xs md:text-sm lg:text-lg font-bold uppercase tracking-widest '
             >
               Logo
             </Link>
           </div>
 
           {/* Nav links */}
-          <ul className='flex items-center gap-2 sm:gap-6 lg:gap-10'>
+          <ul className='flex items-center gap-2 sm:gap-6 lg:gap-10 text-[6px] sm:text-xs md:text-sm lg:text-lg'>
             
-            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-[3px] sm:text-sm lg:text-lg font-medium uppercase tracking-wider  '>
+            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  font-medium uppercase tracking-wider  '>
               <Link href='/Brunch'>Brunch</Link>
             </li>
-            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-[3px] sm:text-sm lg:text-lg font-medium uppercase tracking-wider   '>
+            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  font-medium uppercase tracking-wider   '>
               <Link href='/Location'>Location</Link>
             </li>
-            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-[3px] sm:text-sm lg:text-lg font-medium uppercase tracking-wider  '>
+            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  font-medium uppercase tracking-wider  '>
               <Link href='/Salon_de_the'>Salon De Thé</Link>
             </li>
-            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-[3px] sm:text-sm lg:text-lg font-medium uppercase tracking-wider  '>
+            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  font-medium uppercase tracking-wider  '>
               <Link href='/Tapas'>Tapas</Link>
             </li>
-            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 text-[3px] sm:text-sm lg:text-lg font-medium uppercase tracking-wider  '>
+            <li className=' transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300  font-medium uppercase tracking-wider  '>
                 <Link href='/A_Propos'>A Propos</Link>
             </li>
           </ul>
