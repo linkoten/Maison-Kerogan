efb80e62c6c0bc855e0146ab5cf12fb6d2cd8668
linkoten <catto.francois@hotmail.fr>
2025-10-06 15:38:06 +0200

    Page Contact pour la version mobile

--- src/components/layout/test.jsx
+++ src/components/layout/test.jsx
@@ -13,33 +13,36 @@ import {
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../../../public/format texte/blanc.jpg";
-
 import { usePathname } from "next/navigation";
 
 const Test = () => {
   const pathname = usePathname();
 
   return (
-    <nav className=" fixed z-30 bg-white h-20 w-full sm:hidden">
+    <nav className="fixed z-30 bg-white h-20 w-full sm:hidden">
       <Sheet>
         <SheetTrigger className="flex">
-          <div className=" p-8 px-8 space-y-2 cursor-pointer pl-4 ">
+          <div className="p-8 px-8 space-y-2 cursor-pointer pl-4">
             <span className="block w-8 h-[2px] bg-ocre"></span>
             <span className="block w-4 h-[2px] bg-ocre"></span>
             <span className="block w-8 h-[2px] bg-ocre"></span>
           </div>
           <Image
-            className=" my-auto object-cover mx-auto  h-1/2 w-1/2   "
+            className="my-auto object-cover mx-auto h-1/2 w-1/2"
             alt="maison_kerogan"
             src={logo}
           />
         </SheetTrigger>
         <SheetContent>
           <SheetHeader>
-            <SheetDescription className="flex flex-col h-full  text-lg space-y-20 pt-12 px-8 text-black uppercase ">
+            {/* AJOUT DU TITRE POUR L'ACCESSIBILITÉ */}
+            <SheetTitle className="sr-only">
+              Menu de navigation Maison Kerogan
+            </SheetTitle>
+            <SheetDescription className="flex flex-col h-full text-lg space-y-20 pt-12 px-8 text-black uppercase">
               <SheetClose asChild>
                 <Link
-                  className={`link  ${
+                  className={`link ${
                     pathname === "/"
                       ? "text-ocre border-b border-ocre scale-125 hover:ocre"
                       : "border-b border-vert hover:text-vert hover:scale-110 hover:bold"
@@ -51,10 +54,10 @@ const Test = () => {
               </SheetClose>
               <SheetClose asChild>
                 <Link
-                  className={`link  ${
+                  className={`link ${
                     pathname === "/Brunch"
                       ? "text-ocre border-b border-ocre scale-125 hover:ocre"
-                      : "border-b border-vert hover:text-vert hover:scale-110  hover:bold"
+                      : "border-b border-vert hover:text-vert hover:scale-110 hover:bold"
                   }`}
                   href="/Brunch"
                 >
@@ -63,10 +66,10 @@ const Test = () => {
               </SheetClose>
               <SheetClose asChild>
                 <Link
-                  className={`link  ${
+                  className={`link ${
                     pathname === "/Salon_de_the"
-                      ? "text-ocre border-b border-ocre scale-125 hover:ocre "
-                      : "border-b border-vert hover:text-vert hover:scale-110  hover:bold"
+                      ? "text-ocre border-b border-ocre scale-125 hover:ocre"
+                      : "border-b border-vert hover:text-vert hover:scale-110 hover:bold"
                   }`}
                   href="/Salon_de_the"
                 >
@@ -75,27 +78,38 @@ const Test = () => {
               </SheetClose>
               <SheetClose asChild>
                 <Link
-                  className={`link  ${
+                  className={`link ${
                     pathname === "/Tapas"
                       ? "text-ocre border-b border-ocre scale-125 hover:ocre"
-                      : "border-b border-vert hover:text-vert hover:scale-110  hover:bold"
+                      : "border-b border-vert hover:text-vert hover:scale-110 hover:bold"
                   }`}
                   href="/Tapas"
                 >
                   After Work
                 </Link>
               </SheetClose>
-
               <SheetClose asChild>
                 <Link
-                  className={`link  ${
+                  className={`link ${
                     pathname === "/Location"
                       ? "text-ocre border-b border-ocre scale-125 hover:ocre"
-                      : "border-b border-vert hover:text-vert hover:scale-110  hover:bold"
+                      : "border-b border-vert hover:text-vert hover:scale-110 hover:bold"
                   }`}
                   href="/Location"
                 >
                   évènement
+                </Link>
+              </SheetClose>
+              <SheetClose asChild>
+                <Link
+                  className={`link ${
+                    pathname === "/Contact"
+                      ? "text-ocre border-b border-ocre scale-125 hover:ocre"
+                      : "border-b border-vert hover:text-vert hover:scale-110 hover:bold"
+                  }`}
+                  href="/Contact"
+                >
+                  Contact
                 </Link>
               </SheetClose>
             </SheetDescription>
